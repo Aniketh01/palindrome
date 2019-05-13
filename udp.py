9e4e2e4b2e90a64f1921c61de7807c5f0a101e40
Vaishnav Sivadas <vaishnavsivadas@gmail.com>
2019-05-14 04:32:17 +0530

    Updated udp.py

--- udp.py
+++ udp.py
@@ -1,1 +1,16 @@
-print("ainh")
+import sys
+import socket
+
+def send_packet():
+    #while(1):
+    UDP_IP = sys.argv[1]
+    UDP_PORT = 5005
+    MESSAGE = sys.argv[2]
+    sock = socket.socket(socket.AF_INET, # Internet
+    socket.SOCK_DGRAM) # UDP
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    print ("UDP target IP:", UDP_IP)
+    print ("UDP target port:", UDP_PORT)
+#print "message:", MESSAGE
+
+send_packet()
