a563123e2d28882e7b240e90893c8ad45582ff93
Aniketh01 <anikethgireesh@gmail.com>
2019-05-14 05:07:18 +0530

    parsed and send the udp packet

--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 from .forms import addIPForm
+from .models import IP
+from django.core.management import call_command
 
 
 class add_IP(generic.CreateView):
@@ -24,6 +28,14 @@ class add_IP(generic.CreateView):
 
             text = form.cleaned_data['ipaddr']
             form = addIPForm()
+
+            value = str(IP.objects.latest('id')).split(':')
+            addr = value[0]
+            mode = value[1]
+
+            cmd = 'python3 udp.py' + " " + addr + " " + mode
+
+            os.system(cmd)
 
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
