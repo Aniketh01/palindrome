4011fc203ac37822d22d92c4668b9fbe688d3cdd
vibhoothi <vibhoothiiaanand@gmail.com>
2019-05-22 12:29:32 +0000

    logger: Add more conditions for SMS Alert

--- logger/views.py
+++ logger/views.py
@@ -15,36 +15,44 @@ from twilio.rest import Client
 
 
 class add_logger(generic.CreateView):
-    template_name = 'logger.html'
+	template_name = 'logger.html'
 
-    def get(self, request):
-        form = loggerForm()
-        return render(request, self.template_name, {'form': form})
-    
-    def post(self, request):
-        form= logger(request.POST)
-        args = {'form': form, 'text': text}
-        return render(request, self.template_name, args)
-    
-    def search(request):
-        if 'q' in request.GET:
-            account_sid = "AC5b9acec6a476697f62191cf40a5d842d"
-            auth_token  = "594f63482e4560300f6a9a6c20452dfe"
-            client = Client(account_sid, auth_token)
-            message = client.messages.create(
-                body="Mamaaaa please?! I love you <3",
-                to="+917012418862",
-                from_="+12564729923",
-            )
-            print(message.sid)
-            message = 'You searched for: %r' % request.GET['q']
-        else:
-            message = 'You submitted an empty form.'
-        return HttpResponse(message)
-    
-    def profile_page(request, username=None):
-        user = User.objects.get(username=username)
-        message = request.GET.get('message')
+	def get(self, request):
+		form = loggerForm()
+		return render(request, self.template_name, {'form': form})
 
-    def search_form(request):
-        return render(request, 'logger/search-form.html')
+	def post(self, request):
+		form= logger(request.POST)
+		args = {'form': form, 'text': text}
+		return render(request, self.template_name, args)
+
+	def search(request):
+		if 'q' in request.GET:
+			account_sid = "AC5b9acec6a476697f62191cf40a5d842d"
+			auth_token  = "594f63482e4560300f6a9a6c20452dfe"
+	 		client = Client(account_sid, auth_token)
+	 		if 'intruder' in request.GET['q']:
+	 			message = client.messages.create(
+				 body="Intruder alert!! Motion detected",
+				 to="+917012418862",
+				 from_="+12564729923",
+				 )
+			print(message.sid)
+			if 'fire' in request.GET['q']:
+				message = client.messages.create(
+					body="Fire alert!! Sudden spike in temperature",
+					to="+917012418862",
+					from_="+12564729923",
+					)
+				print(message.sid)
+			message = 'You searched for: %r' % request.GET['q']
+		else:
+			message = 'You submitted an empty form.'
+		return HttpResponse(message)
+
+	 def profile_page(request, username=None):
+		 user = User.objects.get(username=username)
+		 message = request.GET.get('message')
+
+	def search_form(request):
+			 return render(request, 'logger/search-form.html')
